ad37475af95d942e148080dfb90426320a3fe51d
Suph Prajamsri <suphprajam95@gmail.com>
2021-01-16 12:40:51 -0800

    Create files

---
+++ components/Hero.jsx
@@ -0,0 +1,7 @@
+import styles from '../styles/components/Hero.module.scss'
+
+const Hero = () => {
+    return <div>hero</div>
+}
+
+export default Hero
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,9 +1,11 @@
+import Hero from '../components/Hero'
 import styles from '../styles/pages/Home.module.scss'
 
 export default function Home() {
     return (
         <div className={styles.container}>
             <h1 className='t-heading'>Homepage</h1>
+            <Hero />
         </div>
     )
 }
