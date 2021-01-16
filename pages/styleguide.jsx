1f9a0a8603e52f1a86c9742a826ba7c06d9aab88
Scott Nguyen <scottqmn@gmail.com>
2021-01-16 12:13:58 -0800

    Styleguide component/page

---
+++ pages/styleguide.jsx
@@ -0,0 +1,12 @@
+import Styleguide from '../components/Styleguide'
+
+const StyleguidePage = () => {
+    return (
+        <div>
+            <h1 className='t-heading'>Styleguide</h1>
+            <Styleguide />
+        </div>
+    )
+}
+
+export default StyleguidePage
