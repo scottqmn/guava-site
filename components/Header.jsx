32799f8872446e39ab839029d59407b490470bb7
Suph Prajamsri <suphprajam95@gmail.com>
2021-01-24 17:21:03 -0800

    sticky position and handles icon

--- components/Header.jsx
+++ components/Header.jsx
@@ -2,18 +2,37 @@ import clsx from 'clsx'
 import styles from '../styles/components/Header.module.scss'
 
 const Header = () => {
+    const links = [
+        { href: '/', name: 'Home', image: '/images/icon.png' },
+        {
+            href: 'https://www.google.com',
+            name: 'Google',
+        },
+        { href: 'https://www.apple.com/', name: 'Apple' },
+    ]
     return (
         <div className={clsx(styles.container, 'outer')}>
-            <div className={clsx('inner')}> </div>
-            <div className={styles.links}>
-                <div className={styles.linkContainer}>
-                    <a href='url' className='t-button'>
-                        link1
-                    </a>
-                    <a href='url' className='t-button'>
-                        link2
-                    </a>
-                </div>
+            <div className={clsx(styles.linkContainer, 'inner')}>
+                {links.map((linkData) => {
+                    const { href, name, image } = linkData
+                    return (
+                        <a
+                            key={name}
+                            href={href}
+                            className={clsx(styles.links, 't-button')}
+                        >
+                            {image ? (
+                                <img
+                                    className={styles.icon}
+                                    src={image}
+                                    alt={name}
+                                />
+                            ) : (
+                                name
+                            )}
+                        </a>
+                    )
+                })}
             </div>
         </div>
     )
