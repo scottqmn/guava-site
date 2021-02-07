ea3ce3c76db05852538847d44d08a88cef243061
Suph Prajamsri <suphprajam95@gmail.com>
2021-02-06 16:23:57 -0800

    Fixed footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -4,30 +4,40 @@ import styles from '../styles/components/Footer.module.scss'
 
 const Footer = () => {
     const links = [
-        { href: '', name: 'Copyright © All rights reserved.' },
+        // { href: '', name: 'Copyright © All rights reserved.' },
         { href: '/privacypolicy', name: 'Privacy Policy' },
         { href: '/termsofuse', name: 'Terms of Use' },
         { href: '/legal', name: 'Legal' },
-        { herf: '/countryoforigin', name: 'United States' },
+        // { herf: '/countryoforigin', name: 'United States' },
     ]
     return (
         <>
             <div className={clsx(styles.outer, 'outer')}>
-                <div className={clsx(styles.Container, 'inner')}>
-                    {links.map((linkData) => {
-                        const { href, name } = linkData
-                        return (
-                            <div key={name}>
-                                {href ? (
-                                    <Link href={href}>
-                                        <a>{name}</a>
-                                    </Link>
-                                ) : (
-                                    name
-                                )}
-                            </div>
-                        )
-                    })}
+                <div className={clsx(styles.container, 'inner')}>
+                    <div className={styles.wrap}>
+                        <div>Copyright © All rights reserved.</div>
+                        <div className={clsx(styles.linkContainer)}>
+                            {links.map((linkData) => {
+                                const { href, name } = linkData
+                                return (
+                                    <div key={name}>
+                                        {href ? (
+                                            <Link href={href}>
+                                                <a>{name}</a>
+                                            </Link>
+                                        ) : (
+                                            name
+                                        )}
+                                    </div>
+                                )
+                            })}
+                        </div>
+                        <div>
+                            <Link href='/countryoforigin'>
+                                <a>USA</a>
+                            </Link>
+                        </div>
+                    </div>
                 </div>
             </div>
         </>
