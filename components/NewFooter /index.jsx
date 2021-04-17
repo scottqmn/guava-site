e29a72f9596bfe0ea638d1f53ce83eb886ff8bc3
Cathy Dao <cathydao95@gmail.com>
2021-04-16 19:08:29 -0700

    footer component

---
+++ components/NewFooter /index.jsx
@@ -0,0 +1,28 @@
+import StyleGuide from '../Styleguide'
+import clsx from 'clsx'
+import styles from './styles.module.scss'
+
+const NewFooter = () => {
+    return (
+        //outer and inner explanation?//
+        <div className={styles.footer}>
+            <div className={styles.container}></div>
+            <div className={styles.row}>
+                <a href='/about' className={styles.text}>
+                    About
+                </a>
+                <a href='/work' className={styles.text}>
+                    Work
+                </a>
+                <a href='/contact' className={styles.text}>
+                    Contact
+                </a>
+            </div>
+            <div className={styles.copyright}>
+                <p>© 2021 Guava Site</p>
+            </div>
+        </div>
+    )
+}
+
+export default NewFooter
