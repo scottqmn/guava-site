e131e8cdd10330cbd23bff26db8312f45e9bfd1b
Suph Prajamsri <suphprajam95@gmail.com>
2021-04-26 22:08:32 -0700

    fixed grid layout for testimonial

--- components/Testimonial/index.jsx
+++ components/Testimonial/index.jsx
@@ -1,5 +1,6 @@
 import clsx from 'clsx'
 import styles from './styles.module.scss'
+import Testimony from '../Testimony'
 
 const Testimonial = () => {
     return (
@@ -8,12 +9,18 @@ const Testimonial = () => {
                 <div className={clsx(styles.title, 't-h2')}>Testimonial</div>
                 <div className={styles.line} />
             </div>
-            <div>
-                <img
-                    src='/images/testimonial-1.png'
-                    alt='basket of fruits'
-                    className={styles.img}
-                />
+            <div className={styles.grid}>
+                <div className={styles.imgWrap}>
+                    <img
+                        src='/images/testimonial-1.png'
+                        alt='basket of fruits'
+                        className={styles.img}
+                    />
+                </div>
+
+                <div className={styles.testimony}>
+                    <Testimony />
+                </div>
             </div>
         </>
     )
---
+++ components/Testimony/index.jsx
@@ -0,0 +1,22 @@
+import clsx from 'clsx'
+import styles from './styles.module.scss'
+import Star from '../../public/icons/star.svg'
+
+const Testimony = () => {
+    return (
+        <>
+            <div>
+                <div>
+                    <Star />
+                </div>
+                <div> review </div>
+                <div> picture</div>
+            </div>
+            <div>
+                <div>previous</div>
+            </div>
+        </>
+    )
+}
+
+export default Testimony
