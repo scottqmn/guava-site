8579607a1f05157504c4c2a9c9447c7d62bb025b
Cathy Dao <cathydao95@gmail.com>
2021-04-27 19:54:14 -0700

    i13

--- components/Layout/index.jsx
+++ components/Layout/index.jsx
@@ -1,7 +1,7 @@
 import PropTypes from 'prop-types'
 import Header from '../Header'
 import Footer from '../Footer'
-//import NewFooter from '../NewFooter'
+import NewFooter from '../NewFooter'
 
 const Layout = ({ children }) => {
     return (
