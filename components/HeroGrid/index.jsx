6f5ecc7abb272a5a0b788bf793edb05654f32851
Scott Nguyen <scottqmn@gmail.com>
2021-04-29 22:08:21 -0700

    Cleanup homepage, move herogrid data

--- components/HeroGrid/index.jsx
+++ components/HeroGrid/index.jsx
@@ -3,34 +3,60 @@ import clsx from 'clsx'
 import Hero from '../Hero'
 import styles from './styles.module.scss'
 
-// Page component (React component used by next js)
-const HeroGrid = ({ content }) => {
+const CONTENT = [
+    {
+        heading: 'Nokia 21',
+        caption: 'Blast past fast.',
+        body: (
+            <>
+                From $29.12/mo. or $699 before trade-in.
+                <br />
+                Buy directly from Cat with special carrier offers.
+            </>
+        ),
+        image: '/images/hero-1.png',
+        // dark: false,
+    },
+    {
+        heading: 'TCL 21',
+        caption: "It's a leap year.",
+        body: (
+            <>
+                From $41.62/mo. or $999 before trade-in.
+                <br />
+                Buy directly from Apple with special carrier offers.
+            </>
+        ),
+        image: '/images/hero-2.png',
+        dark: true,
+    },
+    {
+        heading: 'Sidekick 21',
+        caption: 'Fast Blast to the Past',
+        body: (
+            <>
+                From $63.82/mo. or $1299 before trade-in.
+                <br />
+                Buy directly from Apple with special carrier offers.
+            </>
+        ),
+        image: '/images/hero-3.png',
+        // dark: false,
+        bottomAlign: true,
+    },
+]
+
+const HeroGrid = ({ content = CONTENT }) => {
     return (
         <div className={clsx(styles.container, 'full-width')}>
             <div className={styles.heroGrid}>
                 {content.map((heroData) => {
-                    const {
-                        heading,
-                        caption,
-                        body,
-                        dark,
-                        image,
-                        bottomAlign,
-                    } = heroData
                     return (
                         <div
-                            key={heading} // identifier for react to keep track of Hero components
+                            key={heroData.heading} // unique identifier for react to keep track of Hero components
                             className={styles.heroGridItem}
                         >
-                            <Hero
-                                {...heroData}
-                                // heading={heading}
-                                // caption={caption}
-                                // body={body}
-                                // dark={dark}
-                                // image={image}
-                                // bottomAlign={bottomAlign}
-                            />
+                            <Hero {...heroData} />
                         </div>
                     )
                 })}
